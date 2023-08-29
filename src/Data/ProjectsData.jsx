b973714309c2fecb36bae30b74d96e6f656b563c
Gynsora <djamaninadjim@gmail.com>
2023-08-29 14:24:01 +0200

    modification  projectData

--- src/Data/ProjectsData.jsx
+++ src/Data/ProjectsData.jsx
@@ -20,27 +20,5 @@ export const ProjectsListData = [
         "demoLink":"https://verre-image.com/",
         "frameworks":"Photoshop Wordpress",
         "genre":["infographie"]
-    },
-    {
-        "id":"a-0017",
-        "name":"mon portfolio",
-        "image": "img/ndz_portfolio.png",
-        "alt":"page d'accueil du portfolio en ligne de nadjim",
-        "description":"Ce site est mon Portfolio, il a réalisé après avoir étudié ReactJs en auto-didacte",
-        "codeLink":"https://github.com/gynsora/Portoflio_DJAMANI_Nadjim",
-        "demoLink":"",
-        "frameworks":"React SCSS",
-        "genre":["web"]
-    },
-    {
-        "id":"a-078",
-        "name":"verre&image",
-        "image": "img/verreEtImage.png",
-        "alt":"page d'accueil de verre-&-image",
-        "description":"Verre&Image est un site internet permettant aux professionnels d'obtenir des plaques funéraires entièrement personnalisée en couleur.",
-        "codeLink":"",
-        "demoLink":"https://verre-image.com/",
-        "frameworks":"Photoshop Wordpress",
-        "genre":["web"]
     }
 ] 
