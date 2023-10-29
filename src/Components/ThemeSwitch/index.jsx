13fd88120cf899c60a4a2973b33c1cfed0d27d4c
Gynsora <djamaninadjim@gmail.com>
2023-10-29 17:52:58 +0100

    Modification de ThemeSwitch

--- src/Components/ThemeSwitch/index.jsx
+++ src/Components/ThemeSwitch/index.jsx
@@ -1,18 +1,30 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { ThemeContext , ColorContext} from '../../Utils/Context';
 import './ThemeSwitch.scss';
 
 function ThemeSwitch(){
     const { theme, toggleTheme } = useContext(ThemeContext);
     const { toggleColor } = useContext(ColorContext);
+
+    const [stateThemeWindow, setStateThemeWindow] = useState('close');
+    const toggleStateThemeWindow = () => {
+           if (stateThemeWindow === 'close'){setStateThemeWindow('open')}
+           else {setStateThemeWindow('close')}     
+    };
+
     return (
-        <div id="theme-switch">
-                <div className="cercle" id="theme-swtich-red" onClick={() => toggleColor('red')}></div>
-                <div className="cercle" id="theme-swtich-green" onClick={() => toggleColor('green')} ></div>
-                <div className="cercle" id="theme-swtich-blue" onClick={() => toggleColor('blue')}></div>
-                <div id="moon-start">
-                    <i id="bar" className={theme === 'light' ? 'fa-regular fa-moon' :'fa-regular fa-sun' } onClick={() => toggleTheme()}></i>
-                </div>
+        <div id="theme-switch-container" className={stateThemeWindow === 'close' ? 'close' :'open' }>
+            <div id="show-theme-switch" onClick={() => toggleStateThemeWindow()}>
+                <i class="fa-solid fa-gear"></i>
+            </div>
+            <div id="theme-switch">
+                    <div className="cercle" id="theme-swtich-red" onClick={() => toggleColor('red')}></div>
+                    <div className="cercle" id="theme-swtich-green" onClick={() => toggleColor('green')} ></div>
+                    <div className="cercle" id="theme-swtich-blue" onClick={() => toggleColor('blue')}></div>
+                    <div id="moon-start">
+                        <i id="bar" className={theme === 'light' ? 'fa-regular fa-moon' :'fa-regular fa-sun' } onClick={() => toggleTheme()}></i>
+                    </div>
+            </div>
         </div>
     )
 }
